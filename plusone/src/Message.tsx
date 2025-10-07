ca2023db351497a4fe6b49c1b25e8e1890a2d17c
sz237 <sarah.d.zeng@vanderbilt.edu>
2025-10-07 14:42:38 -0500

    Built a locally hosted React app

---
+++ plusone/src/Message.tsx
@@ -0,0 +1,7 @@
+// PascalCasing - capitalizing the first letter of each word
+function Message() {
+    // JSX - JavaScript XML, a syntax extension for JavaScript
+    return <h1>Hello, World!</h1>;
+}
+
+export default Message;

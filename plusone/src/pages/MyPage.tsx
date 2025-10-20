872a957ebb9cdf1b8b45c025479355b414c8c4ee
sz237 <156171059+sz237@users.noreply.github.com>
2025-10-20 16:07:58 -0500

    Update import statement for Post type

--- plusone/src/pages/MyPage.tsx
+++ plusone/src/pages/MyPage.tsx
@@ -2,7 +2,6 @@ import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import { postService } from "../services/postService";
-import type { Post } from "../services/postService";
 import type { Post } from "../types/post";
 
 //const GOLD = "#F2E1C0";
